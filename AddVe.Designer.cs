3aa6a00edf3f12491b7a1a986d7c130553dbb164
sanh nguyen <sanhnguyen734@gmail.com>
2022-05-17 04:27:12 +0700

    17_5

--- AddVe.Designer.cs
+++ AddVe.Designer.cs
@@ -37,7 +37,7 @@ namespace DoAnRapChieuPhim
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbx_ve = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -46,7 +46,14 @@ namespace DoAnRapChieuPhim
             this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.comboBox5 = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -61,7 +68,7 @@ namespace DoAnRapChieuPhim
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Location = new System.Drawing.Point(12, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 1;
@@ -70,7 +77,7 @@ namespace DoAnRapChieuPhim
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Location = new System.Drawing.Point(12, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(46, 13);
             this.label3.TabIndex = 2;
@@ -81,14 +88,14 @@ namespace DoAnRapChieuPhim
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(3, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Mã lịch chiếu :";
+            this.label4.Text = "Lịch chiếu";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 268);
+            this.label5.Location = new System.Drawing.Point(12, 352);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(59, 13);
             this.label5.TabIndex = 4;
@@ -97,7 +104,7 @@ namespace DoAnRapChieuPhim
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 324);
+            this.label6.Location = new System.Drawing.Point(12, 408);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(49, 13);
             this.label6.TabIndex = 5;
@@ -106,7 +113,7 @@ namespace DoAnRapChieuPhim
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 379);
+            this.label7.Location = new System.Drawing.Point(12, 463);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(57, 13);
             this.label7.TabIndex = 6;
@@ -115,29 +122,30 @@ namespace DoAnRapChieuPhim
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 434);
+            this.label8.Location = new System.Drawing.Point(12, 518);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(44, 13);
             this.label8.TabIndex = 7;
             this.label8.Text = "Giá vé :";
             // 
-            // textBox1
+            // tbx_ve
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 17);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 20);
-            this.textBox1.TabIndex = 8;
+            this.tbx_ve.Location = new System.Drawing.Point(113, 17);
+            this.tbx_ve.Name = "tbx_ve";
+            this.tbx_ve.ReadOnly = true;
+            this.tbx_ve.Size = new System.Drawing.Size(150, 20);
+            this.tbx_ve.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(113, 431);
+            this.textBox2.Location = new System.Drawing.Point(113, 515);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(150, 20);
             this.textBox2.TabIndex = 9;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(113, 373);
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 457);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(150, 20);
             this.dateTimePicker1.TabIndex = 10;
@@ -145,7 +153,7 @@ namespace DoAnRapChieuPhim
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(113, 134);
+            this.comboBox1.Location = new System.Drawing.Point(113, 113);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(150, 21);
             this.comboBox1.TabIndex = 11;
@@ -153,7 +161,7 @@ namespace DoAnRapChieuPhim
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(113, 73);
+            this.comboBox2.Location = new System.Drawing.Point(113, 65);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(150, 21);
             this.comboBox2.TabIndex = 12;
@@ -161,15 +169,15 @@ namespace DoAnRapChieuPhim
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(85, 3);
+            this.comboBox3.Location = new System.Drawing.Point(6, 16);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(150, 21);
+            this.comboBox3.Size = new System.Drawing.Size(105, 21);
             this.comboBox3.TabIndex = 13;
             // 
             // comboBox4
             // 
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(113, 265);
+            this.comboBox4.Location = new System.Drawing.Point(113, 349);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(150, 21);
             this.comboBox4.TabIndex = 14;
@@ -177,25 +185,83 @@ namespace DoAnRapChieuPhim
             // comboBox5
             // 
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(113, 321);
+            this.comboBox5.Location = new System.Drawing.Point(113, 405);
             this.comboBox5.Name = "comboBox5";
             this.comboBox5.Size = new System.Drawing.Size(150, 21);
             this.comboBox5.TabIndex = 15;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.comboBox6);
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.comboBox3);
             this.panel1.Location = new System.Drawing.Point(12, 171);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(328, 77);
+            this.panel1.Size = new System.Drawing.Size(543, 172);
             this.panel1.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(117, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(289, 166);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(412, 16);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(128, 21);
+            this.comboBox6.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(412, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Phim";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(18, 566);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(112, 98);
+            this.btn_add.TabIndex = 17;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(222, 566);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(112, 98);
+            this.btn_edit.TabIndex = 18;
+            this.btn_edit.Text = "UPDATE";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(424, 566);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(112, 98);
+            this.btn_del.TabIndex = 19;
+            this.btn_del.Text = "DELETE";
+            this.btn_del.UseVisualStyleBackColor = true;
             // 
             // AddVe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(757, 521);
+            this.ClientSize = new System.Drawing.Size(567, 676);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.comboBox4);
@@ -203,7 +269,7 @@ namespace DoAnRapChieuPhim
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbx_ve);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -213,8 +279,10 @@ namespace DoAnRapChieuPhim
             this.Controls.Add(this.label1);
             this.Name = "AddVe";
             this.Text = "AddVe";
+            this.Load += new System.EventHandler(this.AddVe_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,7 +298,7 @@ namespace DoAnRapChieuPhim
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbx_ve;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ComboBox comboBox1;
@@ -239,5 +307,11 @@ namespace DoAnRapChieuPhim
         private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.ComboBox comboBox5;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_del;
     }
 }
--- AddVe.cs
+++ AddVe.cs
@@ -12,9 +12,49 @@ namespace DoAnRapChieuPhim
 {
     public partial class AddVe : Form
     {
-        public AddVe()
+        int state;
+        string mave, manv, makh, malc, madoan, maghe;
+
+        private void AddVe_Load(object sender, EventArgs e)
+        {
+            switch (state)
+            {
+                case 1: ///ADD
+                    {
+                        btn_del.Enabled = false;
+                        btn_del.Visible = false;
+                        btn_edit.Enabled = false;
+                        btn_edit.Visible = false;
+                        tbx_ve.Text = "VE" + DateTime.Now.Month.ToString()
+                                          + DateTime.Now.Day.ToString()
+                                          + DateTime.Now.Hour.ToString()
+                                          + DateTime.Now.Minute.ToString()
+                                          + DateTime.Now.Second.ToString();
+                        break;
+
+                    }
+                case 2: ///DEL update
+                    {
+                        btn_add.Enabled = false;
+                        btn_add.Visible = false;
+                        break;
+                    }
+            }
+        }
+
+        DateTime ngaydat;
+        int giave;
+        public AddVe(int state, string mave, string makh, string malc, string madoan, string maghe, DateTime ngaydat, int giave)
         {
             InitializeComponent();
+            this.state = state;
+            this.mave = mave;
+            this.makh = makh;
+            this.malc = malc;
+            this.madoan = madoan;
+            this.maghe = maghe;
+            this.ngaydat = ngaydat;
+            this.giave = giave;
         }
     }
 }
